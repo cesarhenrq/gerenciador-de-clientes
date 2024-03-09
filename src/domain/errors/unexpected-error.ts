94d6a60f001e0c5585856bea13c917341404c944
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 18:35:57 -0300

    feat: add UnexpectedError class to handle unexpected errors

---
+++ src/domain/errors/unexpected-error.ts
@@ -0,0 +1,6 @@
+export default class UnexpectedError extends Error {
+  constructor() {
+    super("Algo de errado aconteceu. Tente novamente em breve.");
+    this.name = "UnexpectedError";
+  }
+}
