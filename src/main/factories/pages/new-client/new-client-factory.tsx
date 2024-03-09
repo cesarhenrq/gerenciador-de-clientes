b3a8cc59cabe6262c6473b8303d18a0e46a64756
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 20:28:49 -0300

    feat: add error handling to handleSubmit function in new-client-factory.tsx

--- src/main/factories/pages/new-client/new-client-factory.tsx
+++ src/main/factories/pages/new-client/new-client-factory.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 import { NewClient } from "@presentation/components/pages";
 
 import { ISearch } from "@presentation/interfaces";
 
+import { makeRemoteClients } from "@main/usecases/clients";
+
 const MakeNewClient = () => {
-  const handleSubmit = (data: ISearch) => {
-    console.log(data);
+  const [error, setError] = useState<string>("");
+
+  const handleSubmit = async (data: ISearch) => {
+    try {
+      const remoteClients = makeRemoteClients();
+      await remoteClients.post(data);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setError(error.message);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
   };
-  return <NewClient handleSubmit={handleSubmit} />;
+  return <NewClient handleSubmit={handleSubmit} error={error} />;
 };
 
 export default MakeNewClient;
