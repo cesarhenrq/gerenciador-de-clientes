c8e745d6a10306d48975887f269b7a9979bfc768
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:50:41 -0300

    feat: add data property to IProps interface

--- src/presentation/components/pages/clients/props.ts
+++ src/presentation/components/pages/clients/props.ts
@@ -1,5 +1,6 @@
-import { ISearch } from "@presentation/interfaces";
+import { ISearch, IClient } from "@presentation/interfaces";
 
 export default interface IProps {
   handleSearch: (search: ISearch) => void;
+  data: Omit<IClient, "address">[];
 }
