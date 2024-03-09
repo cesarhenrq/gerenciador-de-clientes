efb609cf130487081d506c453b89b2d5121a405e
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:04:28 -0300

    feat: add styles for clients page

---
+++ src/presentation/components/pages/clients/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import { Box } from "@mui/material";
+
+export const Container = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+`;
