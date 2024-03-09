702f24b77d9fbce4e5f5cba038669b9aba485e38
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:51:14 -0300

    refactor: update search bar styles

--- src/presentation/components/ui/search-bar/styles.ts
+++ src/presentation/components/ui/search-bar/styles.ts
@@ -4,9 +4,14 @@ import { Box } from "@mui/material";
 
 export const Container = styled(Box)`
   display: flex;
+  justify-content: space-between;
   gap: 16px;
 
   width: 100%;
 
   margin-bottom: 16px;
+
+  @media (min-width: 768px) {
+    justify-content: flex-start;
+  }
 `;
