2f4e366be03cd26c60ef3879b04cc84e557cc4e4
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:50:21 -0300

    feat: add sample data to Clients page

--- src/main/factories/pages/clients/clients-factory.tsx
+++ src/main/factories/pages/clients/clients-factory.tsx
@@ -2,6 +2,16 @@ import { Clients } from "@presentation/components/pages";
 
 import { ISearch } from "@presentation/interfaces";
 
+function createData(name: string, email: string, phone: string) {
+  return { name, email, phone };
+}
+
+const rows = [
+  createData("Frozen yoghurt", "frozen@email.com", "123456789"),
+  createData("Ice cream sandwich", "ice@email@com", "987654321"),
+  createData("Eclair", "eclar@email.com", "123456789"),
+];
+
 const MakeClients = () => {
   const handleSearch = (search: ISearch) => {
     console.log("searching...", search);
@@ -9,6 +19,7 @@ const MakeClients = () => {
 
   const props = {
     handleSearch,
+    data: rows as any,
   };
 
   return <Clients {...props} />;
