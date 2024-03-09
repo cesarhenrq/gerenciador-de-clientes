d6efbfc8519e18abb9501d9c64eb2025630186d9
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 18:37:07 -0300

    feat: add IResponse interface

--- src/presentation/interfaces/index.ts
+++ src/presentation/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { default as ISearch } from "./search";
 export type { default as IClient } from "./client";
 export type { default as IAddress } from "./address";
+export type { default as IResponse } from "./response";
