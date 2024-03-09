6119f9bf0198da2798a43b7477512d4740f4e7fa
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:05:22 -0300

    feat: add text-fields-factory component for search bar

---
+++ src/presentation/components/ui/search-bar/utils/text-fields-factory.tsx
@@ -0,0 +1,41 @@
+import React, { memo } from "react";
+
+import { TextField } from "@mui/material";
+
+import { ISearch } from "@presentation/interfaces";
+
+interface IProps {
+  search: ISearch;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const MakeTextFields = memo(({ search, handleChange }: IProps) => {
+  const inputs = [
+    {
+      label: "Nome",
+      name: "name",
+    },
+    {
+      label: "Email",
+      name: "email",
+    },
+    {
+      label: "Telefone",
+      name: "phone",
+    },
+  ];
+
+  return inputs.map((input) => (
+    <TextField
+      key={input.name}
+      id='outlined-search'
+      label={input.label}
+      type='search'
+      name={input.name}
+      value={search[input.name as keyof typeof search]}
+      onChange={handleChange}
+    />
+  ));
+});
+
+export default MakeTextFields;
