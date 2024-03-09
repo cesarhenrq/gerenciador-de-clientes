2d474bb30a8c27768fb761d3af8c2803df2ac0d3
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:05:14 -0300

    feat: add styles for search bar component

---
+++ src/presentation/components/ui/search-bar/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import { Box } from "@mui/material";
+
+export const Container = styled(Box)`
+  display: flex;
+  gap: 16px;
+
+  width: 100%;
+
+  margin-bottom: 16px;
+`;
