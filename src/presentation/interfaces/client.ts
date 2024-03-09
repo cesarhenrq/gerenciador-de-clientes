1ae10e81257f105856f8a53ead81af834b008787
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 20:31:52 -0300

    remove: unused import and properties from IClient interface

--- src/presentation/interfaces/client.ts
+++ src/presentation/interfaces/client.ts
@@ -1,10 +1,6 @@
-import IAddress from "./address";
-
 export default interface IClient {
   id: number;
   name: string;
   email: string;
   phone: string;
-  createdAt: string;
-  address: IAddress;
 }
