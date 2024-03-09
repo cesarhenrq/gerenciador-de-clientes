4d1b0b1236345ccae5c7cfe94fd4e41b8df74ea0
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 14:50:31 -0300

    feat: add table and button to Clients page

--- src/presentation/components/pages/clients/index.tsx
+++ src/presentation/components/pages/clients/index.tsx
@@ -1,13 +1,22 @@
-import { SearchBar } from "@presentation/components/ui";
+import { Link } from "react-router-dom";
+
+import { SearchBar, Table } from "@presentation/components/ui";
 
 import * as S from "./styles";
 
 import IProps from "./props";
+import { Button } from "@mui/material";
 
-const Clients = ({ handleSearch }: IProps) => {
+const Clients = ({ handleSearch, data }: IProps) => {
   return (
     <S.Container>
       <SearchBar onSearch={handleSearch} />
+      <Table data={data} />
+      <Link to='/new-client'>
+        <Button variant='contained' color='primary'>
+          Adicionar Cliente
+        </Button>
+      </Link>
     </S.Container>
   );
 };
