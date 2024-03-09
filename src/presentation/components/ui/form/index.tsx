586ab09d9d69aabfbe53b7a7dc0fc4920a52cef6
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 19:32:44 -0300

    feat: add Form component

---
+++ src/presentation/components/ui/form/index.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+import { Box, Button, TextField } from "@mui/material";
+
+import * as S from "./styles";
+
+import IProps from "./props";
+
+const Form = ({ onSubmit }: IProps) => {
+  const [fields, setFields] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(fields);
+  };
+
+  return (
+    <Box
+      component='form'
+      noValidate
+      autoComplete='off'
+      style={{
+        width: "100%",
+        marginBottom: "36px",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <S.Container>
+        <TextField
+          id='outlined-search-name'
+          label='Nome'
+          type='search'
+          style={{
+            width: "100%",
+          }}
+          value={fields.name}
+          name='name'
+          onChange={handleChange}
+        />
+        <TextField
+          id='outlined-search-email'
+          label='Email'
+          type='search'
+          style={{
+            width: "100%",
+          }}
+          name='email'
+          value={fields.email}
+          onChange={handleChange}
+        />
+        <TextField
+          id='outlined-search-phone'
+          label='Telefone'
+          type='search'
+          style={{
+            width: "100%",
+          }}
+          name='phone'
+          value={fields.phone}
+          onChange={handleChange}
+        />
+        <Button variant='contained' color='success' type='submit'>
+          Cadastrar
+        </Button>
+      </S.Container>
+    </Box>
+  );
+};
+
+export default Form;
