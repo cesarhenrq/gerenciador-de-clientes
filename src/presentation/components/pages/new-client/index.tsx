f3d946942c3c278068e49084c606c14747db4f39
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 19:32:06 -0300

    feat: add new client page component

---
+++ src/presentation/components/pages/new-client/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+import { Typography, Button } from "@mui/material";
+
+import { Form } from "@presentation/components/ui";
+
+import * as S from "./styles";
+
+import IProps from "./props";
+
+const NewClient = ({ handleSubmit }: IProps) => {
+  return (
+    <S.Container>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          alignSelf: "flex-start",
+          alignItems: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        <Link to='/clients'>
+          <Button variant='contained' color='warning'>
+            Voltar
+          </Button>
+        </Link>
+        <Typography variant='h5'>Cadastrar novo cliente</Typography>
+      </div>
+
+      <Form onSubmit={handleSubmit} />
+    </S.Container>
+  );
+};
+
+export default NewClient;
