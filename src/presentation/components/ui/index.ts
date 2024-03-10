39b9ab695699cf2716f27314e592eca07dee5fa3
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-10 05:32:19 -0300

    feat: add DeliveryModal component

--- src/presentation/components/ui/index.ts
+++ src/presentation/components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as SearchBar } from "./search-bar";
 export { default as Table } from "./table";
 export * from "./headers";
 export { default as Form } from "./form";
+export { default as DeliveryModal } from "./modal";
