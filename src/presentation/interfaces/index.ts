80ff1e90087a6543cb33b3a78e2c92c9f62f11ff
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-10 05:32:51 -0300

    feat: remove IAddress interface and add IRoute interface

--- src/presentation/interfaces/index.ts
+++ src/presentation/interfaces/index.ts
@@ -1,4 +1,4 @@
 export type { default as ISearch } from "./search";
 export type { default as IClient } from "./client";
-export type { default as IAddress } from "./address";
 export type { default as IResponse } from "./response";
+export type { default as IRoute } from "./route";
